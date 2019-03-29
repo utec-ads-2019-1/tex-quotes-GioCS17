6fc400f1dcfe2c04a8c8d7bcb963923e82940ec9
Gio <giordano.alvitez@utec.edu.pe>
2019-03-28 21:07:46 -0500

    Solution UVA 272

--- main.cpp
+++ main.cpp
@@ -3,5 +3,25 @@
 using namespace std;
 
 int main(int argc, char *argv[]) {
+
+	string line;
+	bool faro=true,cont=true;
+	while(getline(cin,line)){
+			for(char &a:line){
+				if(a=='\"'){
+					if(faro)
+						cout<<"``";
+					else
+						cout<<"''";
+					faro=!faro;
+				}
+				else
+					cout<<a;
+			}
+		cout<<endl;
+	}
+
     return 0;
 }
+
+
